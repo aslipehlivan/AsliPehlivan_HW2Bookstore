3802f584d2f01393e9760eed447b3bc78ff4a3dd
aslı pehlivan <m.asli.pehlivan@gmail.com>
2020-04-22 23:21:33 +0300

    Problem occured when updating database.

---
+++ WebApplication1/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
